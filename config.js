48081bb2ec5b485e3ca2dd4b42c93592e464cfb0
Julien Ternet <snoupy18@gmail.com>
2020-03-27 17:36:57 +0100

    perm fix

--- config.js
+++ config.js
@@ -14,12 +14,12 @@ const config = {
       name: "Mod",
       check: message => {
         try {
-          const modRole = message.guild.roles.find(
+          const modRole = message.guild.roles.cache.find(
             r => r.name.toLowerCase() === message.settings.modRole.toLowerCase()
           );
           if (
-            (modRole && message.member.roles.has(modRole.id)) ||
-            message.member.hasPermission("MANAGE_MESSAGES", "KICK_MEMBERS")
+            (modRole && message.member.roles.cache.has(modRole.id)) ||
+            message.member.hasPermission(["MANAGE_MESSAGES", "KICK_MEMBERS"])
           )
             return true;
         } catch (e) {
@@ -32,14 +32,16 @@ const config = {
       name: "Staff",
       check: message => {
         try {
-          const adminRole = message.guild.roles.find(
+          const adminRole = message.guild.roles.cache.find(
             r =>
               r.name.toLowerCase() === message.settings.adminRole.toLowerCase()
           );
           if (
-            (adminRole && message.member.roles.has(adminRole.id)) ||
-            message.member.hasPermission("MANAGE_GUILD", "BAN_MEMBERS") ||
-            message.member.hasPermission("ADMINISTRATOR")
+            (adminRole && message.member.roles.cache.has(adminRole.id)) ||
+            message.member.hasPermission(["MANAGE_GUILD", "BAN_MEMBERS"], {
+              checkAdmin: true,
+              checkOwner: false
+            })
           )
             return true;
         } catch (e) {
