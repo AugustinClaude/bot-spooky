966b0fca047d935c4edd19f09d4cdd9fc43ac050
Julien Ternet <snoupy18@gmail.com>
2020-03-30 23:41:02 +0200

    setlogs fix

--- commands/setlogs.js
+++ commands/setlogs.js
@@ -19,7 +19,7 @@ class Setlogs extends Command {
     });
   }
 
-  run(message, args) {
+  async run(message, args) {
     if (!args[0]) {
       return message.channel.send(
         ":x: Veuillez préciser le channel où vous voulez setup vos logs !"
@@ -72,7 +72,7 @@ class Setlogs extends Command {
 
       let getGuildSetting = `SELECT * FROM guildSettings WHERE guildId = '${message.guild.id}';`;
 
-      db.query(getGuildSetting, function(err, results, fields) {
+      db.query(getGuildSetting, async function(err, results, fields) {
         if (err) console.log(err.message);
         //console.log(results);
         if (results[0] == undefined) return;
@@ -81,28 +81,71 @@ class Setlogs extends Command {
             ":warning: Ce salon est déjà défini comme channel de logs !"
           );
 
-        // Finalisation
-        if (channel)
-          message.channel.send(
-            `:white_check_mark: Le channel <#${channel.id}> a bien été défini comme channel de logs !`
-          );
-      });
-
-      // Stockage de channel dans base de donnée
-
-      let setLogChannelId = `UPDATE guildSettings SET logChannel_id = '${channel.id}' WHERE guildSettings.guildId = '${message.guild.id}';`;
-
-      db.query(setLogChannelId, function(err, results, fields) {
-        if (err) {
-          console.log(err.message);
+        let logChannelText;
+        let logChannelConf;
+        if (
+          results[0].logChannel_id == undefined ||
+          results[0].logChannel_name == undefined
+        ) {
+          logChannelText = `ℹ️ Vous n'avez pas de channel de logs. Voulez-vous le setup pour <#${channel.id}> ?`;
+          logChannelConf =
+            "ℹ️ Vous n'avez pas setup de channel de logs. Il n'y en a donc aucun.";
+        } else {
+          logChannelText = `ℹ️ Voulez-vous remplacer le channel de logs <#${results[0].logChannel_id}> **(ancien)** par <#${channel.id}> **(nouveau)** ?`;
+          logChannelConf = `ℹ️ Le channel de logs reste donc l'ancien : <#${results[0].logChannel_id}>`;
         }
-      });
 
-      let setLogChannelName = `UPDATE guildSettings SET logChannel_name = '${channel.name}' WHERE guildSettings.guildId = '${message.guild.id}';`;
+        // Finalisation
+        if (channel) {
+          const m = await message.channel.send(logChannelText);
 
-      db.query(setLogChannelName, function(err, results, fields) {
-        if (err) {
-          console.log(err.message);
+          try {
+            m.react("✅");
+            m.react("❌");
+            const filter = (reaction, user) => {
+              return (
+                ["✅", "❌"].includes(reaction.emoji.name) &&
+                user.id === message.author.id
+              );
+            };
+
+            m.awaitReactions(filter, {
+              max: 1,
+              time: 30000,
+              errors: ["time"]
+            }).then(collected => {
+              const reaction = collected.first();
+              if (reaction.emoji.name === "✅") {
+                // Stockage de channel dans base de donnée
+
+                let setLogChannelId = `UPDATE guildSettings SET logChannel_id = '${channel.id}' WHERE guildSettings.guildId = '${message.guild.id}';`;
+
+                db.query(setLogChannelId, function(err, results, fields) {
+                  if (err) {
+                    console.log(err.message);
+                  }
+                });
+
+                let setLogChannelName = `UPDATE guildSettings SET logChannel_name = '${channel.name}' WHERE guildSettings.guildId = '${message.guild.id}';`;
+
+                db.query(setLogChannelName, function(err, results, fields) {
+                  if (err) {
+                    console.log(err.message);
+                  }
+                });
+
+                return message.channel.send(
+                  `:white_check_mark: Le channel <#${channel.id}> a bien été défini comme channel de logs !`
+                );
+              } else {
+                return message.channel.send(logChannelConf);
+              }
+            });
+          } catch (e) {
+            return message.channel.send(
+              ":x: Je n'ai pas la permission d'ajouter des réactions ! J'ai donc gardé l'ancien channel de logs"
+            );
+          }
         }
       });
     }
