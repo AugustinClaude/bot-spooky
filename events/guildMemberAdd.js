b6ded886b701829048cd153217d8261718816984
Julien Ternet <snoupy18@gmail.com>
2020-04-02 23:51:03 +0200

    ajout stats channel (membres + bots)

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,30 @@
+module.exports = class {
+  constructor(client) {
+    this.client = client;
+  }
+
+  run(member) {
+    const SpookyGuild = {
+      guildID: "695020415294242856",
+      memberCountID: "695033979627110404",
+      botCountID: "695034011067875378"
+    };
+
+    if (member.guild.id == SpookyGuild.guildID) {
+      try {
+        this.client.channels.cache
+          .get(SpookyGuild.memberCountID)
+          .setName(`👤 Members : ${member.guild.memberCount}`);
+        this.client.channels.cache
+          .get(SpookyGuild.botCountID)
+          .setName(
+            `🤖 Bots : ${
+              member.guild.members.cache.filter(m => m.user.bot).size
+            }`
+          );
+      } catch (e) {
+        return;
+      }
+    }
+  }
+};
